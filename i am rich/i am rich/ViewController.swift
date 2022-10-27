f6c79516d0e37a71fec56b9fc5044583c682a2d5
minxbox2@gmail.com <>
2022-10-27 18:26:33 +0900

    first commit

---
+++ i am rich/i am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am rich
+//
+//  Created by NHIT on 2022/10/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
